d79f6c80e20597ef544e6eafed1335c1991a3789
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-02-19 12:39:28 -0500

    Add rating to movie

---
+++ src/MvcMovie/Data/Migrations/20170219172730_Rating.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Data.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Movie",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Movie",
+                newName: "ID");
+        }
+    }
+}
--- src/MvcMovie/Models/SeedData.cs
+++ src/MvcMovie/Models/SeedData.cs
@@ -25,6 +25,7 @@ namespace MvcMovie.Models
                          Title = "When Harry Met Sally",
                          ReleaseDate = DateTime.Parse("1989-1-11"),
                          Genre = "Romantic Comedy",
+                         Rating = "R",
                          Price = 7.99M
                      },
 
@@ -33,6 +34,7 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters ",
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
+                         Rating = "R",
                          Price = 8.99M
                      },
 
@@ -41,16 +43,18 @@ namespace MvcMovie.Models
                          Title = "Ghostbusters 2",
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
+                         Rating = "R",
                          Price = 9.99M
                      },
 
-                   new Movie
-                   {
-                       Title = "Rio Bravo",
-                       ReleaseDate = DateTime.Parse("1959-4-15"),
-                       Genre = "Western",
-                       Price = 3.99M
-                   }
+                     new Movie
+                     {
+                         Title = "Rio Bravo",
+                         ReleaseDate = DateTime.Parse("1959-4-15"),
+                         Genre = "Western",
+                         Rating = "R",
+                         Price = 3.99M
+                     }
                 );
                 context.SaveChanges();
             }
